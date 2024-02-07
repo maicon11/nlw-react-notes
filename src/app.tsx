7dd0ab17d16bd7a98e7faef4404c437c765d6a42
Maicon Lima <mai.ky.3000@gmail.com>
2024-02-06 22:34:18 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+export function App() {
+
+  return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt='nlw expert'/>
+      <form className='w-full'>
+        <input
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          type="text"
+          placeholder='Busque as suas notas...'
+          />
+      </form>
+
+      <div className='h-px bg-slate-700'></div>
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <NewNoteCard/>
+
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/note-card.tsx
@@ -0,0 +1,11 @@
+export function NoteCard() {
+    return (
+        <button className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 text-left focus-visible:ring-2 focus-visible:ring-lime-400 outline-none'>
+            <span className='text-sm font-medium text-slate-300'>Há 2 dias</span>
+            <p className='text-sm leading-6 text-slate-400'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia explicabo recusandae expedita accusantium labore eveniet placeat ut fuga aliquam quia, iure necessitatibus. Ex unde similique tempore reprehenderit, impedit debitis?
+            </p>
+            <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+        </button>
+    )
+}
