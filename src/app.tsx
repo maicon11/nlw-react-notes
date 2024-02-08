0b1b2951a4f57e8a5a703bfcdcb28a280d425d0e
Maicon Lima <mai.ky.3000@gmail.com>
2024-02-07 22:46:34 -0300

    feat: aula 2 nlw

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,10 @@ import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 export function App() {
 
+  const note = {
+    date: new Date(),
+    content: 'Hello World'
+  }
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt='nlw expert'/>
@@ -19,8 +23,8 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard/>
 
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
       </div>
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,63 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { toast } from 'sonner'
+
 export function NewNoteCard() {
-    return (
-        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [content, setContent] = useState('')
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+  
+  function handleContentChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    const value = e.target.value
+    setContent(value)
+    if(value == '') setShouldShowOnboarding(true)
+  }
+
+  function handleSaveNote(e: FormEvent) {
+    e.preventDefault()
+    console.log('oiio')
+    toast.success('Nota criada com sucesso!')
+  }
+
+  return (
+      <Dialog.Root>
+        <Dialog.Trigger className='flex flex-col rounded-md bg-slate-700 p-5 text-left gap-3 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none'>
           <span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
           <p className='text-sm leading-6 text-slate-400'>
             Grave uma nota em áudio que será convertida para texto automaticamente.
           </p>
-        </div>
-    )
+        </Dialog.Trigger>
+        <Dialog.Portal>
+              <Dialog.Overlay className='inset-0 fixed bg-black/60' />
+              <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[648px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+                  <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+                      <X className='size-5'/>
+                  </Dialog.Close>
+                  <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+                    <div className='flex flex-1 flex-col gap-3 p-5'>
+                        <span className='text-sm font-medium text-slate-300'>
+                          Adicionar nota
+                        </span>
+                        {shouldShowOnboarding ? (
+                          <p className='text-sm leading-6 text-slate-400'>
+                          Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em audio ou se preferir utilize <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>apenas texto</button>.
+                        </p>
+                        ): (
+                          <textarea
+                          autoFocus
+                          className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                          onChange={handleContentChange}
+                          />
+                        )}
+                    </div>
+                    <button
+                    className='w-full bg-lime-400 py-4 text-center text-small text-lime-950 outline-none font-medium hover:bg-lime-500'>Salvar nota</button>
+                  </form>
+              </Dialog.Content>
+          </Dialog.Portal>
+      </Dialog.Root>
+  )
 }
